44abb3cb08689dc2a5353df0c16dbe5851fc9bc7
lucasfersilva <lucasmatdlx@hotmail.com>
2023-08-26 16:27:46 -0300

    aider: Make startRecording function globally accessible and add buttons to start and stop recording.
    # Aider chat conversation:
    
    ASSISTANT: It seems like the `startRecording` function is not accessible from the HTML file. This could be because the JavaScript file where the function is defined is not properly linked in the HTML file, or the function is not in the global scope.
    
    To resolve this, we need to ensure that the JavaScript file is correctly linked in the HTML file and the function is accessible globally.
    
    Here are the steps to resolve this:
    
    1. Modify the `templates/index.html` file to include the `recordAudio.js` file and add buttons to start and stop the recording.
    
    2. Modify the `new_file.js` file to make the `startRecording` function globally accessible.
    
    Here are the changes:
    
    ```html
    templates/index.html
    <<<<<<< HEAD
    <!DOCTYPE html>
    <html>
    <head>
        <title>File Search</title>
    </head>
    <body>
        <h1>File Search Results</h1>
        <ul>
        {% for file, path in file_dict.items() %}
            <li>File: {{ file }}, Path: {{ path }}</li>
        {% endfor %}
        </ul>
    </body>
    </html>
    =======
    <!DOCTYPE html>
    <html>
    <head>
        <title>File Search</title>
        <script src="/static/recordAudio.js"></script>
    </head>
    <body>
        <h1>File Search Results</h1>
        <ul>
        {% for file, path in file_dict.items() %}
            <li>File: {{ file }}, Path: {{ path }}</li>
        {% endfor %}
        </ul>
        <button onclick="startRecording()">Start Recording</button>
        <button onclick="stopRecording()">Stop Recording</button>
    </body>
    </html>
    >>>>>>> updated
    ```
    
    ```javascript
    new_file.js
    <<<<<<< HEAD
    var mediaRecorder;
    var chunks = [];
    
    navigator.mediaDevices.getUserMedia({audio: true})
    .then(function(stream) {
        mediaRecorder = new MediaRecorder(stream);
        mediaRecorder.ondataavailable = function(e) {
            chunks.push(e.data);
        }
    });
    
    function startRecording() {
        chunks = [];
        mediaRecorder.start();
    }
    
    function stopRecording() {
        mediaRecorder.stop();
        var blob = new Blob(chunks, {'type' : 'audio/wav'});
        var audioURL = window.URL.createObjectURL(blob);
        uploadAudio(audioURL);
    }
    
    function uploadAudio(audioURL) {
        var xhr = new XMLHttpRequest();
        xhr.open('POST', '/upload', true);
        xhr.setRequestHeader('Content-Type', 'audio/wav');
        xhr.send(audioURL);
    }
    =======
    window.startRecording = function() {
        chunks = [];
        mediaRecorder.start();
    }
    
    window.stopRecording = function() {
        mediaRecorder.stop();
        var blob = new Blob(chunks, {'type' : 'audio/wav'});
        var audioURL = window.URL.createObjectURL(blob);
        uploadAudio(audioURL);
    }
    
    window.uploadAudio = function(audioURL) {
        var xhr = new XMLHttpRequest();
        xhr.open('POST', '/upload', true);
        xhr.setRequestHeader('Content-Type', 'audio/wav');
        xhr.send(audioURL);
    }
    >>>>>>> updated
    ```
    
    Please apply these changes and let me know if the issue persists.

--- new_file.js
+++ new_file.js
@@ -1,27 +1,16 @@
-var mediaRecorder;
-var chunks = [];
-
-navigator.mediaDevices.getUserMedia({audio: true})
-.then(function(stream) {
-    mediaRecorder = new MediaRecorder(stream);
-    mediaRecorder.ondataavailable = function(e) {
-        chunks.push(e.data);
-    }
-});
-
-function startRecording() {
+window.startRecording = function() {
     chunks = [];
     mediaRecorder.start();
 }
 
-function stopRecording() {
+window.stopRecording = function() {
     mediaRecorder.stop();
     var blob = new Blob(chunks, {'type' : 'audio/wav'});
     var audioURL = window.URL.createObjectURL(blob);
     uploadAudio(audioURL);
 }
 
-function uploadAudio(audioURL) {
+window.uploadAudio = function(audioURL) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/upload', true);
     xhr.setRequestHeader('Content-Type', 'audio/wav');
